dc6b7a22bfe457203ed6f32d972564ba0a276ccf
Gio <giorgi.bibilashvili89@gmail.com>
2018-03-22 12:16:13 +0400

    --

---
+++ README.md
@@ -0,0 +1,9 @@
+## Palindrome
+
+Simple function in javascript to check if string is palindrome or not
+
+```javascript
+    function isPalindrome(str){    
+        return str.length > 1 && str == str.split('').reverse().join('');
+    }
+```
--- is_palindrome.js
+++ is_palindrome.js
@@ -1,5 +1,15 @@
 
+let strs = [
+    'adgaagda',
+    'ioatoyotatytest',
+    'ISAPALINILAPASI',
+    '2A3MEAS',
+];
 
 function isPalindrome(str){    
     return str.length > 1 && str == str.split('').reverse().join('');
 }
+
+strs.map( function(str) {
+    console.log(isPalindrome(str))
+ })
